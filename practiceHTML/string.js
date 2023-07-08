a8881e6e67d23026144d717c51e544b0ce5fd150
varshatake <varshagore108@gmail.com>
2023-07-08 16:17:24 +0530

    Html sessions
    All class files of HTML

---
+++ practiceHTML/string.js
@@ -0,0 +1,42 @@
+var demo = "Hello world HELLO JAva";
+
+var demolength = demo.length;
+console.log("length is : ",demolength);
+
+var replacedString = demo.replace("world","goodMorning");
+console.log("Replaced string is :",replacedString);
+
+var concat = demo.concat("good afternoon");
+console.log("Cancated string is :",concat);
+
+var repeat = demo.repeat(4);
+console.log("Repeat is:" , repeat);
+
+var splitedstring = demo.split(" ");
+console.log("split string is:",splitedstring);
+
+var uppercase = demo.toUpperCase();
+console.log("Uppercase string :",uppercase);
+
+var lowercase = demo.toLowerCase();
+console.log("lowercase string :",lowercase);
+
+var localuppercase = demo.toLocaleUpperCase();
+console.log("Local Uppercase string :",localuppercase);
+
+var locallowercase = demo.toLocaleLowerCase();
+console.log("Local lowercase string :",locallowercase);
+
+var val = demo.valueOf();
+console.log("Value of :",val);
+
+var text = "Hello guys , today is mi vs GT match";
+var textstart = text.startsWith("is",19);
+console.log("start with :",textstart);
+
+
+
+
+
+
+
